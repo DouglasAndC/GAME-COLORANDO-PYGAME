3d68df7e69e5e506fad3b21bedd076d66e4db349
Douglas Andrade Cerqueira da Silva <x222357@prservicos.com.br>
2019-10-19 17:41:15 -0300

    mapa finalizado

---
+++ Main/main.py
@@ -0,0 +1,55 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+from Mapa import *
+
+pygame.init()
+SCREEN_SIZE = (1000,400)
+screen = pygame.display.set_mode(SCREEN_SIZE, 0 ,8)
+
+mapa = Mapa('Data\mapa')
+
+moving_right = False
+moving_left = False
+vertical_momentum = 0
+air_timer = 0
+
+true_scroll = [0,0]
+
+player_rect = pygame.Rect(100,100,5,13)
+
+def desenhar_mapa(mapa):
+       tile_rects = []
+       y=0
+       for linha in mapa.value:
+        x = 0
+        for celula in linha:
+            if celula == '1':
+                screen.blit(mapa.img_sub,(x*32,y*32))
+            if celula == '2':
+                screen.blit(mapa.img_sup,(x*32,y*32))
+            if celula == '3':
+                screen.blit(mapa.img_triangulo_p1,(x*32,y*32))
+            if celula == '4':
+                screen.blit(mapa.img_triangulo_p2,(x*32,y*32))
+            if celula == '5':
+                screen.blit(mapa.img_trapezio_p1,(x*32,y*32))
+            if celula == '6':
+                screen.blit(mapa.img_trapezio_p2,(x*32,y*32))
+            if celula == '7':
+                screen.blit(mapa.img_trapezio_p3,(x*32,y*32))
+            if celula != '0':
+                tile_rects.append(pygame.Rect(x*32,y*32,32,32))
+            x += 1
+        y += 1
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+
+    desenhar_mapa(mapa)
+
+    pygame.display.update()
