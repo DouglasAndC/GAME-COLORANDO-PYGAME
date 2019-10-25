bcf8ec833d6e5de152256238e14a98bddce486c4
= <mateus.pinto@inmetrics.com>
2019-10-24 22:32:10 -0300

    Tela de Contatos

--- Main/menu.py
+++ Main/menu.py
@@ -5,7 +5,7 @@ pygame.init()
  
 screen_largura = 800
 screen_altura = 700
-
+cont = 1
 BLACK = (0,0,0)
 BLUE = (0, 0, 255)
 WHITE = (255,255,255)
@@ -53,6 +53,33 @@ def menu():
     screen.blit(dimensao_botao('btnSair'),(300,600))
     return (btn1,btn2,btn3,btn4,btn5)
 
+
+def contatos():
+    aut = pygame.image.load(globals.get_path()+'\\View\\menu\\contatos.png').convert_alpha()
+    novo_aut = pygame.transform.scale(aut,(int(940/2), int(620/2)))
+    screen.fill((0, 0, 0))
+    screen.blit(novo_aut,(250,100))
+    btn6 = botoesMenu(BLACK,300,600,372/2,149/2)
+    screen.blit(dimensao_botao('btnVoltar'),(300,500))
+    
+def primeiro():
+    if menu_botoes[0].collidepoint(pos) and pressed1:
+        jogar()
+        cont=+1
+    if menu_botoes[1].collidepoint(pos) and pressed1:
+        jogar()
+        cont=+1
+    if menu_botoes[2].collidepoint(pos) and pressed1:
+        contatos()
+        cont=+1
+    if menu_botoes[3].collidepoint(pos) and pressed1:
+        jogar()
+        cont=+1
+    if menu_botoes[4].collidepoint(pos) and pressed1:
+        pygame.quit()
+        exit()
+    
+    
 def jogar():
     screen.fill((0, 0, 0))
 
@@ -60,26 +87,14 @@ menu()
 
 pygame.display.flip()
 
+menu_botoes = menu()
+
 while True:
     pos = pygame.mouse.get_pos()
     pressed1,pressed2,pressed3 = pygame.mouse.get_pressed()
-    menu_botoes = menu()
-    if menu_botoes[0].collidepoint(pos) and pressed1:
-        jogar()
-        pygame.display.flip()
-    if menu_botoes[1].collidepoint(pos) and pressed1:
-        jogar()
-        pygame.display.flip()
-    if menu_botoes[2].collidepoint(pos) and pressed1:
-        jogar()
-        pygame.display.flip()
-    if menu_botoes[3].collidepoint(pos) and pressed1:
-        jogar()
-        pygame.display.flip()
-    if menu_botoes[4].collidepoint(pos) and pressed1:
-        pygame.quit()
-        exit()
-
+    if(cont == 1):
+        primeiro()
+    pygame.display.flip()            
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
