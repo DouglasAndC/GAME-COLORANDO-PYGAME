07f1bbc35db18204d7ce6869a80a2149d8de191d
Douglas Andrade Cerqueira da Silva <x222357@prservicos.com.br>
2019-10-27 19:02:41 -0300

    personagem andando

--- Main/main.py
+++ Main/main.py
@@ -6,15 +6,16 @@ from mapa import *
 pygame.init()
 SCREEN_SIZE = (1000,500)
 screen = pygame.display.set_mode(SCREEN_SIZE, 0 ,16)
-
+clock = pygame.time.Clock()
 display = pygame.Surface((400,200))
 
 mapa = mapa('Data\mapa')
 
 moving_right = False
 moving_left = False
-frame_user = 0
-
+frame_user = 1
+x_user = 104
+y_user = 140
 true_scroll = [0,0]
 
 player_rect = pygame.Rect(75,75,5,13)
@@ -46,7 +47,7 @@ def desenhar_mapa(mapa):
 
 while True:
     display.fill((146,244,255))
-    
+    clock.tick(12)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -54,12 +55,14 @@ while True:
     
     display.fill((146,244,255))
     img = pygame.image.load(globals.get_path() + "\\View\\user_"+str(frame_user)+".png").convert_alpha()
-    display.blit(img,(100+globals.speed,140))
-    globals.speed+=1
+    display.blit(img,(x_user+globals.speed,y_user))
+    globals.speed+=4
     if(frame_user == 4):
-        frame_user=0
+        frame_user=2
     else:
         frame_user-=-1
+    if(x_user>=display.x):
+        display+=400,200
     desenhar_mapa(mapa)
     display.blit(mapa.img_sol,(300,10))
     screen.blit(pygame.transform.scale(display,SCREEN_SIZE),(0,0))
