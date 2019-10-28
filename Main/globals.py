ac0108a002ed9efae68353af961c25b42c2bfab3
Douglas Andrade Cerqueira da Silva <x222357@prservicos.com.br>
2019-10-27 22:57:33 -0300

    ordenação da base concluída

--- Main/globals.py
+++ Main/globals.py
@@ -1,4 +1,7 @@
 import os
+import pickle
+import json
+
 def get_path():
     os.chdir(os.getcwd())
     if(os.getcwd().__contains__('\\Main')):
@@ -12,3 +15,28 @@ def convert(s):
     for x in s: 
         new += x  
     return new
+
+def ler_usuario():
+    data = []
+    try:
+        with open(get_path()+'\Data\data.pickle', 'rb') as f:
+            data = pickle.load(f)
+            f.close()
+        return data
+    except Exception as e:
+        print(e)
+    return data
+
+def salvar_usuario(user):
+    data=ler_usuario()
+    data.append(user)
+    if(len(data)>1):
+        data.sort(key=lambda x: x.score, reverse=True)
+    try:
+        with open(get_path()+'\Data\data.pickle', 'wb') as f:
+            pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+            f.close()
+        return True
+    except Exception as e:
+        print(e)
+    return False
--- Main/usuario.py
+++ Main/usuario.py
@@ -1,6 +1,4 @@
-import pickle
 import globals
-import json
 class usuario(object):
 
     def __init__(self, nome):
@@ -10,32 +8,4 @@ class usuario(object):
 
     def __str__(self):
         return "nome: %s; score: %s; level: %i" % (self.nome,self.score,self.level)
-    
-    def ler_usuario(self):
-        data = {}
-        try:
-            with open(globals.get_path()+'\Data\data.pickle', 'rb') as f:
-                data = pickle.load(f)
-            return data
-        except Exception as e:
-            print(e)
-        return data
-    
-    def salvar_usuario(self):
-        user=[{"nome":self.nome,"score":self.score,"level":self.level}]
-        try:
-            with open(globals.get_path()+'\Data\data.pickle', 'wb') as f:
-                pickle.dump(user, f, pickle.HIGHEST_PROTOCOL)
-            return True
-        except Exception as e:
-            print(e)
-        return False
-    
-    def calcular_posicao(self):
-        data = self.ler_usuario()
-        for i in sorted (data.keys().__getitem__('score')) :  
-            print(i, end = " ")
-
-user1 = usuario('Douglas')
-user1.salvar_usuario()
-user1.calcular_posicao()
+        
